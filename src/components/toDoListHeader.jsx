1a3416ff2d8afb7aa53cd95a81b5854585d539a0
XXIBULLDOGIXX <art.vanhandenhoven@gmail.com>
2022-04-14 16:17:12 +0200

    toDoListHeaderFormatted

--- src/components/toDoListHeader.jsx
+++ src/components/toDoListHeader.jsx
@@ -3,5 +3,16 @@ import React, {useState, useEffect} from 'react';
 export default function ToDoListHeader() {
     
 
-    return <h1>noob</h1>
+    return (
+        <div id="toDoListHeaderContainer">
+            <h1 id="titleToDoApp">ToDo App</h1>
+            <form id="addToDoForm" action="/submit">
+                <input id="inputToDo" type="text" />
+                <div id="btnsContainerToDoApp">
+                    <button class="todoBtn" id="addToDoBtn" type="submit">Add ToDo</button>
+                    <button class="todoBtn" id="DeleteAllToDoesBtn" type="reset">Delete all</button>
+                </div>
+            </form>
+        </div>
+    );
 }
